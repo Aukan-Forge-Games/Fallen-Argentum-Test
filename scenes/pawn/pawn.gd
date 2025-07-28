b51e8d0a138703eea3cdefd9e53270c7c6d034fe
Sammy Flemington <samflemington@gmail.com>
2025-07-28 16:09:19 -0500

    Added placeholder sprites and node structure for game board

---
+++ scenes/pawn/pawn.gd
@@ -0,0 +1,22 @@
+extends Node2D
+## Class for game pieces, such as the Player and enemies.
+## Contains functions for moving between squares on the board.
+class_name Pawn
+
+var board_pos : Vector2i
+
+# Use these to control animations
+signal move_started()
+signal move_ended()
+
+@rpc("authority", "call_local", "reliable")
+func move_to_square(pos: Vector2i):
+	move_started.emit()
+	# Create path follow
+	
+	# TODO: Connect follow finished to this
+	move_ended.emit()
+
+## Returns the number of moves needed to reach a given point
+func get_path_distance_to(pos: Vector2i) -> int:
+	return false
